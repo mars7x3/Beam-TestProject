4b0d7fc27f89fe241dee2619f164944f0457ef9b
mars7x3 <m.ysakov.jcc@gmail.com>
2024-03-29 20:13:39 +0600

    done

---
+++ main/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for main project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+
+application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from crud_app.routers import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    'http': application,
+    "websocket": URLRouter(websocket_urlpatterns),
+})
